39618c6e8b0a30c48c1a81ed55136a7cce2e0728
Moumy Ndiaye <moumyndiaye@outlook.com>
2024-03-16 17:02:09 -0400

    Bonne transparence

---
+++ app/services/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIhjSwCh3V78X_tIAk6fGO_42tZcX4YaM",
+  authDomain: "fotosphere.firebaseapp.com",
+  projectId: "fotosphere",
+  storageBucket: "fotosphere.appspot.com",
+  messagingSenderId: "91090247576",
+  appId: "1:91090247576:web:9017e06be0b55c6af915ed",
+};
+export default firebaseConfig;
